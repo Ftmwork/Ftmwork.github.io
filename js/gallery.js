2946202a329935c2363d3bdec53d41c6fb3b76bf
Ftmwork <worka2404@gmail.com>
2026-02-12 12:49:25 +0000

    Update gallery.js

--- js/gallery.js
+++ js/gallery.js
@@ -2,12 +2,24 @@ function upDate(previewPic){
     const images =document.getElementById("image");
     images.style.backgroundImage="url('" + previewPic.src + "')";
     images.textContent=previewPic.alt;
-    
+    console.log("focus activé");
 }
 
-	function unDo(){
-        const images=document.getElementById("image")
-        images.style.backgroundImage="url('')";
-        images.textContent="Hover over an image below to display here.";
+function unDo(){
+    const images=document.getElementById("image")
+    images.style.backgroundImage="url('')";
+    images.textContent="Hover over an image below to display here.";
      
-	}
+}
+
+let tabinex=[]
+
+function aleatoire(){
+    my_images = document.querySelectorAll(".preview")
+    for ( let i=0; i<my_images.length; i++){
+    my_images[i].setAttribute("tabindex",0)
+};
+};
+console.log(aleatoire())
+
+
